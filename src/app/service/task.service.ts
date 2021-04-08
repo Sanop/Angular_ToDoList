987861a00cc72bddf45bd43f50b5475759869941
SanoP <sanodeemantha@gmail.com>
2021-04-08 12:39:15 +0530

    create completed task part

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -8,8 +8,8 @@ export class TaskService {
   taskList : Array<Task> = [];
 
   constructor() {
-    this.taskList.push(new Task("Requirement Gather",false,Priority.PRIORITY1));
-    this.taskList.push(new Task("Design UI",false,Priority.PRIORITY1));
+    this.taskList.push(new Task("Requirement Gather",true,Priority.PRIORITY1));
+    this.taskList.push(new Task("Design UI",true,Priority.PRIORITY1));
     this.taskList.push(new Task("Develop Code",false,Priority.PRIORITY2));
     this.taskList.push(new Task("Test The App",false,Priority.PRIORITY3));
     this.taskList.push(new Task("Build For Production",false,Priority.PRIORITY3));
